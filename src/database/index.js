5e4e68e1dffb5bba222e982dab6b4cf193048941
Sandeep Kumar Jangir <71197636+sandeep7727939593@users.noreply.github.com>
2024-12-21 14:26:58 +0530

    Update index.js

--- src/database/index.js
+++ src/database/index.js
@@ -6,7 +6,8 @@ const configOptions = {
 }
 
 const connectToDB = async () => {
-    const mongoUrl = 'mongodb://localhost:27017/ec2';
+  //  const mongoUrl = 'mongodb://localhost:27017/ec2';
+ const mongoUrl = 'mongodb+srv://sandeep7727939593:sandeep7727939593@ec2.gfbnq.mongodb.net/?retryWrites=true&w=majority&appName=ec2';
     mongoose.connect(mongoUrl, configOptions).then(() => console.log("Mongo Db Connected")).catch(err => console.log(`Error . ${err.message}`))
 }
 
